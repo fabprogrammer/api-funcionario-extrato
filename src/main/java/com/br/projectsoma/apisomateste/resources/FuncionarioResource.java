6c3c43a91340d7f879429507c4ffd5a62b6b09ef
Fabio Leite <fabprogrammer@gmail.com>
2020-03-14 16:58:21 -0300

    primeiro commit do projeto

--- src/main/java/com/br/projectsoma/apisomateste/resources/FuncionarioResource.java
+++ src/main/java/com/br/projectsoma/apisomateste/resources/FuncionarioResource.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,10 +37,16 @@ public class FuncionarioResource {
 		return funcionarioRepository.save(funcionario);
 	}
 	
-	@DeleteMapping("/produto")
+	@DeleteMapping("/funcionario")
 	public void deletaFuncionario(@RequestBody Funcionario funcionario) {
 		funcionarioRepository.delete(funcionario);
 	}
+	
+	@PutMapping("/funcionario")
+	public Funcionario atualizaFuncionario(@RequestBody Funcionario funcionario) {
+		return funcionarioRepository.save(funcionario);
+	}
+	
 	
 	
 	
